fe19891f5bc809d85b2d65e9214b6fbc7d60fa4c
shahar <me@shaharnechmad.com>
2011-02-06 17:02:02 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hobnob::Application.config.secret_token = '93d19a5763a4fcd8001292b7fa215255cdd40a21559bd40d9f0d54d184e9ed0ab1fe2cc9745dae69474eb3ead06e0397643daeb77208410f62ba03902b95bc17'
